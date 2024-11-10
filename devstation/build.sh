038bca73732dfa39adb67438d1f06b9ac79d934b
Matthias Preu <5973515+mpreu@users.noreply.github.com>
2024-11-10 15:18:52 +0100

    Introduce ansible configuration
    - Add ansible configuration for container build
    - Add playbook and roles to set up:
      - System-wide proxy
      - Flatpak
      - Package overlays
      - Gnome
      - Sudo environment

--- devstation/build.sh
+++ devstation/build.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 set -ouex pipefail
 
-### Install packages
+# Get location of this script.
+wd=$(dirname "$(readlink -f "$0")")
 
-rpm-ostree install --idempotent \
-    ansible \
-    python-psutil \
-    gnome-tweak-tool \
-    numix-gtk-theme \
-    numix-icon-theme-circle \
-    guake \
-    git \
-    corkscrew \
-    davfs2
+# Install required ansible packages.
+rpm-ostree install --idempotent ansible python-psutil
+
+# Run ansible.
+pushd "${wd}/ansible"
+    ansible-playbook playbook.yaml
+popd
